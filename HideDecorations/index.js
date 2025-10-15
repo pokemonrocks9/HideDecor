c68c5b8d80eeabc43d9aa4e6dab71b2096a65623
pokemonrocks9 <pokemonrocks9@users.noreply.github.com>
2025-10-15 19:28:33 +0000

    deploy: 6d4ed3fee305e2f76c938786518cb92583ecf105

--- HideDecorations/index.js
+++ HideDecorations/index.js
@@ -1,4 +1,4 @@
-(function(e,o){"use strict";let a=null;var n={onLoad:function(){o.logger.log("Hide Decorations plugin loading..."),a=document.createElement("style"),a.id="hide-decorations-plugin",a.textContent=`
+(function(e,o,l){"use strict";let n=[],t=null;var c={onLoad:function(){t=document.createElement("style"),t.id="hide-decorations-plugin",t.textContent=`
             /* Hide avatar decorations */
             [class*="avatarDecoration"],
             [class*="avatar-decoration"],
@@ -17,4 +17,4 @@
             [class*="userTag"] > [class*="clan"] {
                 display: none !important;
             }
-        `,document.head.appendChild(a),o.logger.log("Hide Decorations plugin loaded! Decorations hidden.")},onUnload:function(){o.logger.log("Hide Decorations plugin unloading..."),a?.parentNode&&(a.parentNode.removeChild(a),a=null),o.logger.log("Hide Decorations plugin unloaded.")}};return e.default=n,Object.defineProperty(e,"__esModule",{value:!0}),e})({},vendetta);
+        `,document.head.appendChild(t);try{const a=l.findByProps("getCurrentUser");a&&n.push(o.after("getCurrentUser",a,function(s,r){return r?.avatarDecoration&&(r.avatarDecoration=null),r}))}catch(a){console.log("Could not patch avatar decorations:",a)}try{const a=l.findByProps("getClan","getTag");a&&a.getClan&&n.push(o.after("getClan",a,function(){return null})),a&&a.getTag&&n.push(o.after("getTag",a,function(){return null}))}catch(a){console.log("Could not patch clan tags:",a)}},onUnload:function(){t?.parentNode&&(t.parentNode.removeChild(t),t=null);for(const a of n)a();n=[]}};return e.default=c,Object.defineProperty(e,"__esModule",{value:!0}),e})({},vendetta.patcher,vendetta.metro);

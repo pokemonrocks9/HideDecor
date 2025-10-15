ae713202448616a9d1429bbc6109fea138d17a8b
pokemonrocks9 <tytygoins@gmail.com>
2025-10-15 16:02:12 -0400

    Update index.ts

--- plugins/HideDecorations/src/index.ts
+++ plugins/HideDecorations/src/index.ts
@@ -1,4 +1,4 @@
-import { findByProps } from '@vendetta/metro';
+import { findByStoreName } from '@vendetta/metro';
 import { instead } from '@vendetta/patcher';
 
 let patches: (() => void)[] = [];
@@ -6,17 +6,22 @@ let patches: (() => void)[] = [];
 export default {
     onLoad: () => {
         try {
-            // TEST 1: Patch getAvatarDecorationURL to return null
-            const ImageResolver = findByProps('getAvatarDecorationURL');
-            if (ImageResolver?.getAvatarDecorationURL) {
+            // TEST 2: Patch UserStore.getUser
+            const UserStore = findByStoreName('UserStore');
+            if (UserStore?.getUser) {
                 patches.push(
-                    instead('getAvatarDecorationURL', ImageResolver, () => {
-                        return null;
+                    instead('getUser', UserStore, (args, orig) => {
+                        const user = orig(...args);
+                        if (user) {
+                            user.avatarDecoration = null;
+                            user.avatarDecorationData = null;
+                        }
+                        return user;
                     })
                 );
-                console.log("[HideDecorations] Test 1: getAvatarDecorationURL patched");
+                console.log("[HideDecorations] Test 2: UserStore.getUser patched");
             } else {
-                console.log("[HideDecorations] Test 1: getAvatarDecorationURL NOT found");
+                console.log("[HideDecorations] Test 2: UserStore.getUser NOT found");
             }
         } catch (error) {
             console.error("[HideDecorations] Error:", error);

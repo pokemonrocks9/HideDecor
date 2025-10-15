fe6629466193074b8e7d4135c2dd3791f43aec96
pokemonrocks9 <pokemonrocks9@users.noreply.github.com>
2025-10-15 19:08:56 +0000

    deploy: 65db4481f71b5a6150646bfe4e4a88907aab8aec

--- HideDecorations/index.js
+++ HideDecorations/index.js
@@ -1,4 +1,4 @@
-(function(e){"use strict";let a=null;var n={onLoad:function(){a=document.createElement("style"),a.id="hide-decorations-plugin",a.textContent=`
+(function(e,o){"use strict";let a=null;var n={onLoad:function(){o.logger.log("Hide Decorations plugin loading..."),a=document.createElement("style"),a.id="hide-decorations-plugin",a.textContent=`
             /* Hide avatar decorations */
             [class*="avatarDecoration"],
             [class*="avatar-decoration"],
@@ -17,4 +17,4 @@
             [class*="userTag"] > [class*="clan"] {
                 display: none !important;
             }
-        `,document.head.appendChild(a)},onUnload:function(){a?.parentNode&&(a.parentNode.removeChild(a),a=null)}};return e.default=n,Object.defineProperty(e,"__esModule",{value:!0}),e})({});
+        `,document.head.appendChild(a),o.logger.log("Hide Decorations plugin loaded! Decorations hidden.")},onUnload:function(){o.logger.log("Hide Decorations plugin unloading..."),a?.parentNode&&(a.parentNode.removeChild(a),a=null),o.logger.log("Hide Decorations plugin unloaded.")}};return e.default=n,Object.defineProperty(e,"__esModule",{value:!0}),e})({},vendetta);

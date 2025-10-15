6d4ed3fee305e2f76c938786518cb92583ecf105
pokemonrocks9 <tytygoins@gmail.com>
2025-10-15 15:28:12 -0400

    Update index.ts

--- plugins/HideDecorations/src/index.ts
+++ plugins/HideDecorations/src/index.ts
@@ -1,12 +1,12 @@
-import { logger } from "@vendetta";
+import { after } from "@vendetta/patcher";
+import { findByProps } from "@vendetta/metro";
 
+let patches: (() => void)[] = [];
 let styleElement: HTMLStyleElement | null = null;
 
 export default {
     onLoad: () => {
-        logger.log("Hide Decorations plugin loading...");
-        
-        // Create and inject CSS to hide decorations
+        // Inject CSS as a fallback method
         styleElement = document.createElement('style');
         styleElement.id = 'hide-decorations-plugin';
         styleElement.textContent = `
@@ -29,20 +29,58 @@ export default {
                 display: none !important;
             }
         `;
-        
         document.head.appendChild(styleElement);
-        logger.log("Hide Decorations plugin loaded! Decorations hidden.");
+
+        // Try to patch avatar decoration rendering
+        try {
+            const UserStore = findByProps("getCurrentUser");
+            if (UserStore) {
+                patches.push(
+                    after("getCurrentUser", UserStore, (args, res) => {
+                        if (res?.avatarDecoration) {
+                            res.avatarDecoration = null;
+                        }
+                        return res;
+                    })
+                );
+            }
+        } catch (e) {
+            console.log("Could not patch avatar decorations:", e);
+        }
+
+        // Try to patch clan tags
+        try {
+            const ClanStore = findByProps("getClan", "getTag");
+            if (ClanStore && ClanStore.getClan) {
+                patches.push(
+                    after("getClan", ClanStore, () => {
+                        return null;
+                    })
+                );
+            }
+            if (ClanStore && ClanStore.getTag) {
+                patches.push(
+                    after("getTag", ClanStore, () => {
+                        return null;
+                    })
+                );
+            }
+        } catch (e) {
+            console.log("Could not patch clan tags:", e);
+        }
     },
 
     onUnload: () => {
-        logger.log("Hide Decorations plugin unloading...");
-        
-        // Remove the style element
+        // Remove CSS
         if (styleElement?.parentNode) {
             styleElement.parentNode.removeChild(styleElement);
             styleElement = null;
         }
-        
-        logger.log("Hide Decorations plugin unloaded.");
+
+        // Unpatch all patches
+        for (const unpatch of patches) {
+            unpatch();
+        }
+        patches = [];
     }
 };
